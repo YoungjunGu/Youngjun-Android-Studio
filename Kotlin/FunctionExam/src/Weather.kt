672106e56484e2a6122efe85e845502227814bc0
gaki2745 <guyeongjun@naver.com>
2019-08-27 16:34:49 +0900

    [Android] Android project init

--- Kotlin/FunctionExam/src/Weather.kt
+++ Kotlin/FunctionExam/src/Weather.kt
@@ -1,3 +1,5 @@
+
+
 fun main(args: Array<String>) {
 
     var fineDust = 100
@@ -5,11 +7,18 @@ fun main(args: Array<String>) {
     var isChina = false
 
 
+
     getCurrentSkyColor(isFresh, fineDust, isChina)
+
 
 
     println(airPollutionSate(fineDust))
     getCurrentWeatherState()
+
+    println({
+        val local = "seoul"
+        "$local 의 현재 날씨는"
+    })
 }
 
 private fun getCurrentSkyColor(isFresh: Boolean, fineDust: Int, isChina: Boolean): String {
@@ -19,8 +28,9 @@ private fun getCurrentSkyColor(isFresh: Boolean, fineDust: Int, isChina: Boolean
     return skyColor
 }
 
-private fun airPollutionSate(fineDust: Int): String {
-    val airCondition = when (fineDust) {
+// 단일 표현식을 사용하여 함수를 변경
+private fun airPollutionSate(fineDust: Int) =
+    when (fineDust) {
         10 -> "최고"
         20 -> "양호"
         30 -> "나쁨"
@@ -28,11 +38,8 @@ private fun airPollutionSate(fineDust: Int): String {
         in 101..130 -> "외출금지"
         else -> "측정불가"
     }
-    return airCondition
-}
 
 private fun getCurrentWeatherState(weatherState: String = "맑음") {
     println("날씨 $weatherState")
 }
-
 
